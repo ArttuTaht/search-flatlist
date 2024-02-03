dd7f67516de16ff1aeb5212d066b847565b3e55e
ArttuTaht <arttu.tahtinen@protonmail.com>
2024-02-03 21:16:44 +0200

    Pushing components

---
+++ Data.js
@@ -0,0 +1,37 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'Jouni'
+    },
+    {
+        id: '2',
+        lastname: 'Ojala',
+        firstname: 'Pekka'
+    },
+    {
+        id: '3',
+        lastname: 'Mikkonen',
+        firstname: 'Ilkka'
+    },
+    {
+        id: '4',
+        lastname: 'Viitala',
+        firstname: 'Matti'
+    },
+    {
+        id: '5',
+        lastname: 'Hynynen',
+        firstname: 'Jouni'
+    },
+    {
+        id: '6',
+        lastname: 'Tähtinen',
+        firstname: 'Arttu'
+    },
+    {
+        id: '7',
+        lastname: 'Tähtinen',
+        firstname: 'Ossi'
+    }
+]
